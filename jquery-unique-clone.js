e07786b44d63b86f9bab5a453d5eacebdfbaed12
Curt Howard <choward@weblinc.com>
2016-05-12 11:15:40 -0400

    Release version 1.0.0

--- jquery-unique-clone.js
+++ jquery-unique-clone.js
@@ -1,6 +1,27 @@
+/**
+ * Clones an element, modifying any [id] or [for] attributes found within the
+ * element's scope to append a given suffix. Modeled after the core `$.clone()`
+ * method.
+ *
+ * @author   Curt Howard <tooblies@gmail.com>
+ * @version  1.0.0
+ *
+ * @param    {Object}    $         the global jQuery object
+ * @param    {Object}    window    the global window object
+ * @param    {Object}    document  the global document object
+ * @param    {undefined} undefined
+ */
 ;(function ($, window, document, undefined) {
     var pluginName = "uniqueClone";
 
+    /**
+     * Plugin boilerplate. Checking and setting of passed arguments.
+     *
+     * @param {Element} element               the target element
+     * @param {Boolean} withDataAndEvents     clone with data and events of element
+     * @param {Boolean} deepWithDataAndEvents clone with data and events of element and it's children
+     * @param {String}  suffix                the string to append to the attribute value
+     */
     function Plugin(element, withDataAndEvents, deepWithDataAndEvents, suffix) {
         this.element = element;
 
@@ -24,6 +45,13 @@
         this.init();
     }
 
+    /**
+     * Clones the element, passing in the first two parameters sent to the
+     * Plugin. Modifies the value of any [id] or [for] attribute found to append
+     * the given suffix.
+     *
+     * @type {Object}
+     */
     Plugin.prototype = {
         init: function() {
             var suffix = this._suffix;
@@ -46,6 +74,20 @@
         },
     };
 
+    /**
+     * Receives three arguments in order to initialize the Plugin. Returns a
+     * property of the Plugin's instance representing the cloned, modified DOM.
+     */
+
+    /**
+     * Public interface for the Plugin. Receives arguments, passing them to the
+     * Plugin's boilerplate for initialization.
+     *
+     * @param  {Boolean} withDataAndEvents     clone with data and events of element
+     * @param  {Boolean} deepWithDataAndEvents clone with data and events of element and it's children
+     * @param  {String}  suffix                the string to append to the attribute value
+     * @return {jQuery}                        a collection containing a cloned, modified version of element
+     */
     $.fn[pluginName] = function (withDataAndEvents, deepWithDataAndEvents, suffix) {
         return (new Plugin(this, withDataAndEvents, deepWithDataAndEvents, suffix))._$clone;
     };
